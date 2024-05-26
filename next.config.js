07357f2fb2612c7899723b8ecff0356903528df7
SalahaldeenAlmamari <salahalmamary684@gmail.com>
2024-05-26 18:45:22 +0300

    ............

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    unoptimized: true,
+// Import the next-pwa package correctly
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true,      // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
 };
 
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWA(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,36 @@
-import "@/styles/globals.css";
-import { motion } from "framer-motion";
-import { Suspense } from "react";
-import { circularProgressClasses } from "@mui/material";
+import Head from 'next/head';
+import { Suspense } from 'react';
+import { circularProgressClasses } from '@mui/material';
 
+import '@/styles/globals.css';
 
+// Define metadata
+const Metadata = {
+  manifest: '/manifest.json',
+  // Add other metadata properties as needed
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="bg-gray-50 text-black">
-      <Suspense  fallback={<circularProgressClasses />} >
+      <Head>
+        {/* Add metadata */}
+        <link rel="manifest" href={Metadata.manifest} />
+        <title>SEO Salah - Your SEO Solution</title>
+        
+        {/* Description */}
+        <meta name="description" content="Improve your SEO and boost your business growth quickly with SEO Salah. Get advanced tools and innovative techniques for maximum online marketing effectiveness." />
+        
+        {/* Keywords */}
+        <meta name="keywords" content="SEO, search engine optimization, marketing, online marketing, digital marketing, SEO tools" />
+        
+        {/* Author */}
+        <meta name="author" content="Salah Aldeen" />
+        
+        {/* Viewport */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Suspense fallback={<circularProgressClasses />}>
         <Component {...pageProps} />
       </Suspense>
     </div>
